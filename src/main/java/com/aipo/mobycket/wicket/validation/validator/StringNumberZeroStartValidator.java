e2497d2ffcdbfc5f43a249e65a76c8bb648779c7
nbeppu <nbeppu@gmail.com>
2012-02-13 13:20:04 +0000

    fixes #16292 mobycket更新

---
+++ src/main/java/com/aipo/mobycket/wicket/validation/validator/StringNumberZeroStartValidator.java
@@ -0,0 +1,26 @@
+/*
+ * Aipo is a groupware program developed by Aimluck,Inc.
+ * Copyright (C) 2004-2011 Aimluck,Inc.
+ * http://aipostyle.com/
+ */
+
+package com.aipo.mobycket.wicket.validation.validator;
+
+import java.util.regex.Pattern;
+
+import org.apache.wicket.validation.validator.PatternValidator;
+
+/**
+ * 先頭が０で始まることを許す、String型の数値Validater
+ */
+public class StringNumberZeroStartValidator extends PatternValidator {
+
+  private static final long serialVersionUID = -4370896421024607370L;
+
+  public static String PATTERN = "^\\d*";
+
+  public StringNumberZeroStartValidator() {
+    super(PATTERN, Pattern.CASE_INSENSITIVE);
+  }
+
+}
